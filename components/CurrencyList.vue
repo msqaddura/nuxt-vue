12afb7c503f1380ebc9ff418da288220419226b2
msqaddura_ <msqaddura@gmail.com>
2021-11-16 23:27:35 +0200

    added bare styles

--- components/CurrencyList.vue
+++ components/CurrencyList.vue
@@ -1,14 +1,42 @@
 <template>
-  <p v-if="$fetchState.pending">Fetching mountains...</p>
-  <p v-else-if="$fetchState.error">An error occurred :(</p>
-  <div v-else>
-    <h1>Data</h1>
-    <ul>
-      <li v-for="item of data" :key="item.name">
-        <NuxtLink :to="`/${item.name}`">{{ item.name }}</NuxtLink>
-      </li>
-    </ul>
-  </div>
+  <section class="container-fluid">
+    <p v-if="$fetchState.pending">Fetching data...</p>
+    <p v-else-if="$fetchState.error">An error occurred :(</p>
+    <div v-else>
+      <ul class="list-unstyled">
+        <li v-for="item of data" :key="item.name" class="jumper media my-3">
+          <NuxtLink
+            :to="`/${item.name}`"
+            class="
+              d-flex
+              align-items-center
+              justify-content-between
+              text-decoration-none
+              w-100
+            "
+          >
+            <div class="media">
+              <img
+                :src="require(`/assets/img/${item.symbol}.png`)"
+                class="align-self-center mr-3"
+              />
+              <div class="media-body">
+                <h5 class="mt-0">{{ item.name }}</h5>
+                <p>
+                  <span>{{ item.amount }}</span>
+                  <span class="text-uppercase">{{ item.symbol }}</span>
+                </p>
+              </div>
+            </div>
+            <div>
+              <span>{{ item.amountEur }}</span
+              ><sup>EUR</sup>
+            </div></NuxtLink
+          >
+        </li>
+      </ul>
+    </div>
+  </section>
 </template>
 
 <script>
@@ -24,3 +52,11 @@ export default {
   },
 }
 </script>
+
+<style scoped lang="scss">
+$size: 32px;
+.media img {
+  width: $size;
+  height: $size;
+}
+</style>
